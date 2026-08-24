b4ec71d56a0d0aee510abe54eede5410e1ab448f
eiswewka2023-a11y <eiswe.wka2023@gmail.com>
2026-08-24 16:52:16 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -172,7 +172,7 @@ def handle_all_messages(message):
             "📞 **ထာဝရ ဆက်သွယ်ရန်**\n\n"
             "• ဖုန်းနံပါတ်: 09797523108\n"
             "• TikTok: https://www.tiktok.com/@sara.eiswe\n\n"
-            "အချိန်မရွေး စာမေးလို့ရပါတယ်နော်!"
+            "အချိန်မရွေး ဆက်သွယ်မေးမြန်းလို့ရပါတယ်နော်!"
         )
         bot.send_message(chat_id, contact_info, reply_markup=get_main_menu())
         return
@@ -183,12 +183,12 @@ def handle_all_messages(message):
             "📍 **'ထာဝရ' မိတ္တူနှင့် ဓါတ်ပုံလုပ်ငန်း**\n\n"
             "🗺️ **Google Maps လိပ်စာ:** https://maps.app.goo.gl/9UPunmNKnJ5R4Ka58\n"
             "📞 **ဆက်သွယ်ရန် ဖုန်း:** 09797523108\n\n"
-            "ဆိုင်သို့ ကြိုဆိုပါတယ် သူငယ်ချင်း!"
+            "'ထာဝရ'မိတ္တူ/ဖိတ်စာ ဆိုင်မှ ကြိုဆိုပါတယ် သူငယ်ချင်းရေ!"
         )
         bot.send_message(chat_id, location, parse_mode="Markdown", reply_markup=get_main_menu())
         return
 
-    elif user_text == "📝 Feedback ပေးမယ်":
+    elif user_text == "📝 Feedback ပေးမယ်(သို့မဟုတ်)ဆိုင်သို့တိုက်ရိုက်ပြောမယ်":
         user_states[chat_id] = "WAITING_FOR_FEEDBACK"
         bot.send_message(
             chat_id, 
